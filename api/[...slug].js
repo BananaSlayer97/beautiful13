4b3653ff031396ff8374132d5c1f4680d793660d
liuya <1039099339@qq.com>
2025-08-09 11:09:06 +0800

    Simplify Vercel config: remove conflicting API routing

--- api/[...slug].js
+++
@@ -1,9 +0,0 @@
-// Vercel API 通配符路由处理文件
-const app = require('../server/app');
-
-// 导出 Vercel serverless 函数
-module.exports = (req, res) => {
-  // 重写URL路径，添加/api前缀
-  req.url = `/api${req.url}`;
-  return app(req, res);
-};
