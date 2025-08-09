f5ab5ca8d21c15a171d852d2ca5c4b7fd7174ae2
liuya <1039099339@qq.com>
2025-08-09 11:05:53 +0800

    Fix API routing: use CommonJS export syntax in slug handler

--- api/[...slug].js
+++ api/[...slug].js
@@ -2,8 +2,8 @@
 const app = require('../server/app');
 
 // 导出 Vercel serverless 函数
-export default function handler(req, res) {
+module.exports = (req, res) => {
   // 重写URL路径，添加/api前缀
   req.url = `/api${req.url}`;
   return app(req, res);
-}
+};
