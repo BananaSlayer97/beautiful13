43abb1c04a07fe9ffd1bd8b4b65c2fff93ca2088
liuya <1039099339@qq.com>
2025-08-09 12:14:20 +0800

    Fix Vercel serverless function export format

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@
 const app = require('../server/app');
 
 // 导出 Vercel serverless 函数
-module.exports = (req, res) => {
+export default function handler(req, res) {
   return app(req, res);
-};
+}
+
+// 兼容 CommonJS
+module.exports = handler;
+module.exports.default = handler;
