6c155b15ec5ea5f88fbfe73540f998bda4ce7d62
liuya <1039099339@qq.com>
2025-08-09 10:52:17 +0800

    Fix Vercel serverless function: export handler function instead of Express app

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // Vercel API 入口文件
 const app = require('../server/app');
 
-// 导出 Vercel 函数
-module.exports = app;
+// 导出 Vercel serverless 函数
+module.exports = (req, res) => {
+  return app(req, res);
+};
